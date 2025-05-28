16d5cec53f326b88f253550d88772f08a1054ff5
Sauhumatti <smsyrja@gmail.com>
2025-05-28 16:50:39 +0300

    Initial commit3

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -106,9 +106,9 @@ export default function Navbar() {
                   Tutustumispeli
                 </Link>
                 <Link
-                  href="/tulokset"
+                  href="/records"
                   className={`px-3 py-2 rounded-lg ${
-                    pathname === "/tulokset"
+                    pathname === "/records"
                       ? "bg-blue-100 text-blue-700"
                       : "text-gray-600 hover:text-gray-900 hover:bg-gray-100"
                   }`}
@@ -190,9 +190,9 @@ export default function Navbar() {
                 Tutustumispeli
               </Link>
               <Link
-                href="/tulokset"
+                href="/records"
                 className={`block px-3 py-2 rounded-lg ${
-                  pathname === "/tulokset"
+                  pathname === "/records"
                     ? "bg-blue-100 text-blue-700"
                     : "text-gray-600 hover:text-gray-900 hover:bg-gray-100"
                 }`}
